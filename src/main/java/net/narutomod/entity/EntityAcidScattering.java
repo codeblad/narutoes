ea71dd4de230aa3f68827134be8f23693625e851
wood-m-corp <148374240+wood-m-corp@users.noreply.github.com>
2026-08-15 18:06:31 -0400

    GitIgnore and Acid Buff
    Adds gradle.properties to gitignore

--- src/main/java/net/narutomod/entity/EntityAcidScattering.java
+++ src/main/java/net/narutomod/entity/EntityAcidScattering.java
@@ -53,7 +53,7 @@ public class EntityAcidScattering extends ElementsNarutomodMod.ModElement {
 		private EntityLivingBase shooter;
 		private float width, range;
 		private int potionAmplifier;
-		public float dmg = 2.01f;
+		public float dmg = 2.04f;
 
 		public EC(World world) {
 			super(world);
