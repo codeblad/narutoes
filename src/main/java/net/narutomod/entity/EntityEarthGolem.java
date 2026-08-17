ebd313a85b18e6a1a0d8fa8abccdf227de405ea7
BRUHSIF38 <bruhsif38@gmail.com>
2026-08-16 20:10:47 -0400

    woops

--- src/main/java/net/narutomod/entity/EntityEarthGolem.java
+++ src/main/java/net/narutomod/entity/EntityEarthGolem.java
@@ -133,7 +133,7 @@ public class EntityEarthGolem extends ElementsNarutomodMod.ModElement {
 		@Override
 		public boolean attackEntityFrom(DamageSource source, float amount) {
 			if (source.getTrueSource() instanceof EntityLivingBase && source.getTrueSource().equals(this.getSummoner())) {
-				this.onDeathUpdate();
+				//this.onDeathUpdate();
 				return false;
 			}
 			return super.attackEntityFrom(source, amount);
