3a722006a0595756e101260ae4e88ed15f7cdd2a
BRUHSIF38 <bruhsif38@gmail.com>
2026-08-23 22:18:16 -0400

    forgot

--- src/main/java/net/narutomod/item/ItemBijuCloak.java
+++ src/main/java/net/narutomod/item/ItemBijuCloak.java
@@ -350,7 +350,7 @@ public class ItemBijuCloak extends ElementsNarutomodMod.ModElement {
 				entity.addPotionEffect(new PotionEffect(PotionReach.potion, 22, level - 1, false, false));
 			}
 			if (entity.getHealth() < entity.getMaxHealth() && entity.getHealth() > 0.0f) {
-				entity.heal((float)level * 0.075f);
+				entity.heal((float)level * 0.15f);
 			}
 			if (entity instanceof  EntityPlayer) {
 				((EntityPlayer) entity).getFoodStats().addStats(1,0);
